866bb7a9fce749aa7c3fc415fca90a1e481df19e
Ed Howard <shapedthought@gmail.com>
2018-09-06 09:29:30 +0100

    JS calculation updates

--- vSAN_Capacity.js
+++ vSAN_Capacity.js
@@ -2,10 +2,15 @@
 
 document.querySelector('#nodeType').addEventListener("change", runFtt);
 
+//Sets current FTT state
+let nodeTypeSelect = 1;
+
 function runFtt() {
 	document.querySelector('#fttHybrid').classList.toggle('d-none');
 	document.querySelector('#fttFlash').classList.toggle('d-none');
 	document.querySelector('#showDedup').classList.toggle('d-none');
+	nodeTypeSelect = nodeTypeSelect === 1 ? 2 : 1;
+	console.log(nodeTypeSelect);
 }
 
 //Add current values to modal on click
@@ -30,25 +35,21 @@ function settingsUpdate() {
 	document.querySelector('#capDiskBaseConsumption').value = capDiskBaseCon;
 };
 
+
 //Save Changes to settings
 
 document.querySelector('#saveSettings').addEventListener("click", saveSettings);
 
 function saveSettings() {
-	slackSpace = document.querySelector('#slackSpace').value;
-	dgBaseCon = document.querySelector('#dgBaseConsumption').value;
-	diskFormat = document.querySelector('#formattingOverhead').value;
-	baseC = document.querySelector('#baseConsumption').value;
-	ssdBaseMemOh = document.querySelector('#SSDMemOverheadPerGiB').value;
-	hybBaseMemOh = document.querySelector('#HDDMemOverheadPerGiB').value;
-	capDiskBaseCon = document.querySelector('#capDiskBaseConsumption').value;
+	slackSpace = parseInt(document.querySelector('#slackSpace').value);
+	dgBaseCon = parseInt(document.querySelector('#dgBaseConsumption').value);
+	diskFormat = parseInt(document.querySelector('#formattingOverhead').value);
+	baseC = parseInt(document.querySelector('#baseConsumption').value);
+	ssdBaseMemOh = parseInt(document.querySelector('#SSDMemOverheadPerGiB').value);
+	hybBaseMemOh = parseInt(document.querySelector('#HDDMemOverheadPerGiB').value);
+	capDiskBaseCon = parseInt(document.querySelector('#capDiskBaseConsumption').value);
 }
 
-//RAM Overhead cal
-function ramOverhead(BC, NDG, DGBC, SMOH, SS, NCD, CDBC) {
-	let overHead = BC + (NDG * (DGBC + (SMOH * SS))) + (NCD * CDBC);
-	return overHead;
-}
 
 //FTT cal
 function fttCapCal(rawCap, format, slack, ftt) {
@@ -58,8 +59,54 @@ function fttCapCal(rawCap, format, slack, ftt) {
 	return postFtt;
 }
 
-//vCPU and RAM deliverable cal
 
-//Minimum host warning based on FTT
+//Submit function
 
-//Maxium disks per group
+document.querySelector('#submit').addEventListener('click', runCal);
+
+function runCal() {
+
+
+	//Virtual Requirements inputs
+	let vcpuReq = parseInt(document.querySelector('#vcpuReq').value);
+	let ramReq = parseInt(document.querySelector('#ramReq').value);
+
+	//Host Qty
+	let hostQuantity = parseInt(document.querySelector('#hostQuantity').value);
+
+	//Host config inputs
+	let overcommit = parseInt(document.querySelector('#overcommit').value);
+	let hostRedundancy = parseInt(document.querySelector('#hostRedundancy').value);
+	let corePerProcessor = parseInt(document.querySelector('#corePerProcessor').value);
+	let processorsPerHost = parseInt(document.querySelector('#processorsPerHost').value);
+	let ramPerHost = parseInt(document.querySelector('#ramPerHost').value);
+
+	//vSAN inputs
+	let fttReduction = 0;
+	document.querySelector('#dedupFactor').defaultValue = 0;
+	if (nodeTypeSelect === 1) {
+		fttReduction = parseFloat(document.querySelector('#fttHybridValue').value);
+	} else {
+		fttReduction = parseFloat(document.querySelector('#fttFlashValue').value);
+	}
+
+	let capacityRequired = parseInt(document.querySelector('#capacityRequired').value);
+	let diskGroupQtyPerHost = parseInt(document.querySelector('#diskGroupQtyPerHost').value);
+	let dataDisksPerDiskGroup = parseInt(document.querySelector('#dataDisksPerDiskGroup').value);
+	let cacheCapacity = parseInt(document.querySelector('#cacheCapacity').value);
+	let dataDiskCapacity = parseInt(document.querySelector('#dataDiskCapacity').value);
+	let dedupFactor = parseFloat(document.querySelector('#dedupFactor').value);
+
+	let baseMemOh = 0;
+
+	if (nodeTypeSelect === 1) {
+		baseMemOh = hybBaseMemOh;
+	} else {
+		baseMemOh = ssdBaseMemOh;
+	}
+
+	console.log(baseC, diskGroupQtyPerHost, dgBaseCon, baseMemOh, cacheCapacity, dataDisksPerDiskGroup, capDiskBaseCon);
+
+	let ramOverhead = baseC + (diskGroupQtyPerHost * (dgBaseCon + (baseMemOh * cacheCapacity))) + (dataDisksPerDiskGroup * capDiskBaseCon);
+	console.log(ramOverhead);
+}
